8751cf64e6d7decfbeb136b36ef34751bb316e2f
Daniel Schröder <daniel.schroeder@gravitymedia.de>
2016-06-29 22:11:41 +0200

    Completed assertions

--- tests/src/UriTest.php
+++ tests/src/UriTest.php
@@ -15,42 +15,44 @@ use GravityMedia\Uri\Uri;
  * @package GravityMedia\UriTest
  *
  * @covers  GravityMedia\Uri\Uri
- * @uses    GravityMedia\Uri\Component\Authority
- * @uses    GravityMedia\Uri\Component\Query
- * @uses    GravityMedia\Uri\Component\UserInfo
  */
 class UriTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Test file URI from string.
+     * Test URI construction without scheme from string.
      */
-    public function testFileUriFromString()
+    public function testUriWithoutSchemeFromString()
     {
-        $expected = '/this/is/a/test/path/to/example.txt';
+        $string = '/this/is/a/test/path/to/example.txt';
+        $uri = Uri::fromString($string);
 
-        $uri = Uri::fromString($expected);
-
-        $this->assertEquals(null, $uri->getScheme());
-        $this->assertEquals('', $uri->getAuthority());
-        $this->assertEquals($expected, $uri->getPath());
-        $this->assertEquals(null, $uri->getFragment());
-        $this->assertEquals($expected, $uri->toString());
+        $this->assertSame('', $uri->getScheme());
+        $this->assertSame('', $uri->getAuthority());
+        $this->assertSame('', $uri->getUserInfo());
+        $this->assertSame('', $uri->getHost());
+        $this->assertSame(null, $uri->getPort());
+        $this->assertSame($string, $uri->getPath());
+        $this->assertSame('', $uri->getQuery());
+        $this->assertSame('', $uri->getFragment());
+        $this->assertSame($string, $uri->toString());
     }
 
     /**
-     * Test HTTP URI from string.
+     * Test HTTP URI construction from string.
      */
     public function testHttpUriFromString()
     {
-        $expected = 'http://user:pass@example.com:8080/this/is/a/test/path?argument=value&array%5B0%5D=1#test_fragment';
+        $string = 'http://user:pass@example.com:8080/this/is/a/test/path?argument=value&array%5B0%5D=1#test_fragment';
+        $uri = Uri::fromString($string);
 
-        $uri = Uri::fromString($expected);
-
-        $this->assertEquals('http', $uri->getScheme());
-        $this->assertEquals('user:pass@example.com:8080', $uri->getAuthority());
-        $this->assertEquals('/this/is/a/test/path', $uri->getPath());
-        $this->assertEquals('argument=value&array%5B0%5D=1', $uri->getQuery());
-        $this->assertEquals('test_fragment', $uri->getFragment());
-        $this->assertEquals($expected, $uri->toString());
+        $this->assertSame('http', $uri->getScheme());
+        $this->assertSame('user:pass@example.com:8080', $uri->getAuthority());
+        $this->assertSame('user:pass', $uri->getUserInfo());
+        $this->assertSame('example.com', $uri->getHost());
+        $this->assertSame(8080, $uri->getPort());
+        $this->assertSame('/this/is/a/test/path', $uri->getPath());
+        $this->assertSame('argument=value&array%5B0%5D=1', $uri->getQuery());
+        $this->assertSame('test_fragment', $uri->getFragment());
+        $this->assertSame($string, $uri->toString());
     }
 }
